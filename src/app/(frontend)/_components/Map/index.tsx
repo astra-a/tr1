001835e59d84fb56a628f45498ef0c99a5f0ac6f
volare-dev <dev@volare.finance>
2025-09-21 21:22:28 +0800

    update map

--- src/app/(frontend)/_components/Map/index.tsx
+++ src/app/(frontend)/_components/Map/index.tsx
@@ -7,7 +7,7 @@ import { Countries } from "../../_mocks/map";
 const GlobeMap = dynamic(() => import("./GlobeMap"), { ssr: false });
 
 function CountriesList() {
-  const [availableArr, waitlistArr, comingSoonArr, maxArr] = useMemo(() => {
+  const [availableArr, waitlistArr, comingSoonArr] = useMemo(() => {
     const _availableArr: string[] = [];
     const _waitlistArr: string[] = [];
     const _comingSoonArr: string[] = [];
@@ -24,46 +24,29 @@ function CountriesList() {
           break;
       }
     });
-    return [
-      _availableArr,
-      _waitlistArr,
-      _comingSoonArr,
-      new Array(
-        Math.max(
-          _availableArr.length,
-          _waitlistArr.length,
-          _comingSoonArr.length,
-        ),
-      ).fill(""),
-    ];
+    return [_availableArr, _waitlistArr, _comingSoonArr];
   }, []);
 
   return (
-    <div className="relative w-full max-w-225 mt-10 border-gradient-rounded line-ray rounded-xl text-base font-semibold text-jet-black">
-      <div className="flex flex-col w-full bg-eerie-black rounded-xl">
-        <div
-          className="py-4.5 -m-0.25 rounded-xl"
-          style={{
-            background: "linear-gradient(90deg, #00FFC2 0%, #7DDAFF 100%)",
-          }}
-        >
-          <div className="flex justify-between px-10">
-            <div className="w-[25%]">Available</div>
-            <div className="w-[25%] text-center">Waitlist</div>
-            <div className="w-[25%] text-right">Coming soon</div>
+    <div className="relative w-full max-w-260 mt-10 border-gradient-rounded line-ray rounded-xl text-base font-semibold text-white">
+      <div className="flex flex-col w-full p-4 bg-eerie-black rounded-xl">
+        <div className="flex w-full py-2 border-b-1 border-b-dark-gray">
+          <div className="p-4 w-35 flex-none">Available</div>
+          <div className="p-4">
+            <p>{availableArr.join(", ")}</p>
           </div>
         </div>
-        <div className="text-white px-10">
-          {maxArr.map((_, i) => (
-            <div
-              key={i}
-              className="flex justify-between py-4 border-b-1 border-b-dark-gray"
-            >
-              <div className="w-[25%]">{availableArr?.[i]}</div>
-              <div className="w-[25%] text-center">{waitlistArr?.[i]}</div>
-              <div className="w-[25%] text-right">{comingSoonArr?.[i]}</div>
-            </div>
-          ))}
+        <div className="flex w-full py-2 border-b-1 border-b-dark-gray">
+          <div className="p-4 w-35 flex-none">Waitlist</div>
+          <div className="p-4">
+            <p>{waitlistArr.join(", ")}</p>
+          </div>
+        </div>
+        <div className="flex w-full py-2">
+          <div className="p-4 w-35 flex-none">Coming soon</div>
+          <div className="p-4">
+            <p>{comingSoonArr.join(", ")}</p>
+          </div>
         </div>
       </div>
     </div>
