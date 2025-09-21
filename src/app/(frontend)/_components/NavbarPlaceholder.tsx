02e526956d8dcb8427067c9d0706aad4dbd7584b
volare-dev <dev@volare.finance>
2025-09-21 16:54:54 +0800

    add map Countries List

--- src/app/(frontend)/_components/NavbarPlaceholder.tsx
+++ src/app/(frontend)/_components/NavbarPlaceholder.tsx
@@ -4,7 +4,7 @@ import { usePathname } from "next/navigation";
 
 export default function NavbarPlaceholder() {
   const pathname = usePathname();
-  return ["/", "/roadmap", "/map"].includes(pathname) ? (
+  return ["/", "/roadmap"].includes(pathname) ? (
     <></>
   ) : (
     <div className="navbar-placeholder w-full h-12 md:h-13 lg:h-14 xl:h-15 2xl:h-18 3xl:h-19.5 4xl:h-32" />
