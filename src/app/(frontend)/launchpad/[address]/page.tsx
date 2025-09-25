34e3452e9c5084b66c0fd0a3631e897d8dfa4490
volare-dev <dev@volare.finance>
2025-09-25 10:21:08 +0800

    responsive: LaunchpadBuy

--- src/app/(frontend)/launchpad/[address]/page.tsx
+++ src/app/(frontend)/launchpad/[address]/page.tsx
@@ -17,9 +17,5 @@ export default async function Page({
     return notFound();
   }
 
-  return (
-    <div className="launchpad-stake w-full max-w-370 min-h-100 mx-auto px-4 sm:px-6 md:px-7 lg:px-8 xl:px-9 2xl:px-10 py-28 relative overflow-hidden antialiased">
-      <BuyAndStake pool={pool} />
-    </div>
-  );
+  return <BuyAndStake pool={pool} />;
 }
