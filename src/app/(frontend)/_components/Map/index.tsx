04550bb265c642bdc09f7ff98dcd9e4f7e4dedaf
volare-dev <dev@volare.finance>
2025-09-26 12:08:18 +0800

    fix: 7 8

--- src/app/(frontend)/_components/Map/index.tsx
+++ src/app/(frontend)/_components/Map/index.tsx
@@ -3,14 +3,14 @@
 import dynamic from "next/dynamic";
 import { useMemo } from "react";
 import { Countries } from "../../_mocks/map";
+import GlowingEdgeCard from "../../_components/GlowingEdgeCard";
 
 const GlobeMap = dynamic(() => import("./GlobeMap"), { ssr: false });
 
 function CountriesList() {
-  const [availableArr, waitlistArr, comingSoonArr] = useMemo(() => {
+  const [availableArr, waitlistArr, columns, rows, width] = useMemo(() => {
     const _availableArr: string[] = [];
     const _waitlistArr: string[] = [];
-    const _comingSoonArr: string[] = [];
     Countries.features.forEach((feature) => {
       switch (feature.properties.STATE) {
         case "available":
@@ -19,36 +19,79 @@ function CountriesList() {
         case "waitlist":
           _waitlistArr.push(feature.properties.ADMIN);
           break;
-        case "coming soon":
-          _comingSoonArr.push(feature.properties.ADMIN);
-          break;
       }
     });
-    return [_availableArr, _waitlistArr, _comingSoonArr];
+    const _columns =
+      _waitlistArr.length / _availableArr.length > 2
+        ? 4
+        : _waitlistArr.length / _availableArr.length > 1
+          ? 3
+          : 2;
+    const _waitlistRows = Math.ceil(_waitlistArr.length / (_columns - 1));
+    const _rows = Math.max(_availableArr.length, _waitlistRows);
+    const _waitlistArr2 = [];
+    for (let i = 0; i < _rows - 1; i++) {
+      _waitlistArr2.push(
+        _waitlistArr.slice(_waitlistRows * i, _waitlistRows * (i + 1)),
+      );
+    }
+    return [
+      _availableArr,
+      _waitlistArr2,
+      _columns,
+      _rows,
+      `${100 / _columns}%`,
+    ];
   }, []);
 
   return (
-    <div className="relative w-full max-w-260 mt-10 border-gradient-rounded line-ray rounded-xl text-base font-semibold text-white">
-      <div className="flex flex-col w-full p-4 bg-eerie-black rounded-xl">
-        <div className="flex w-full py-2 border-b-1 border-b-dark-gray">
-          <div className="p-4 w-35 flex-none">Available</div>
-          <div className="p-4">
-            <p>{availableArr.join(", ")}</p>
+    <div className="relative w-full max-w-270 mt-10 border-gradient-rounded line-ray rounded-xl text-base font-semibold text-jet-black">
+      <GlowingEdgeCard autoPlayOnHover className="w-full h-full">
+        <div className="flex flex-col w-full bg-eerie-black rounded-xl">
+          <div
+            className="py-4.5 -m-0.25 rounded-xl"
+            style={{
+              background: "linear-gradient(90deg, #00FFC2 0%, #7DDAFF 100%)",
+            }}
+          >
+            <div className="flex justify-between px-10">
+              <div className="shrink-0" style={{ width }}>
+                Available
+              </div>
+              {new Array(columns - 1).fill("").map((_, i) => (
+                <div key={i} className="shrink-0" style={{ width }}>
+                  Waitlist
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="text-white px-10">
+            {new Array(rows).fill("").map((_, i) => (
+              <div
+                key={i}
+                className="flex justify-between py-4 border-b-1 border-b-dark-gray"
+              >
+                <div className="shrink-0" style={{ width }}>
+                  {availableArr?.[i]}
+                </div>
+                <div className="shrink-0" style={{ width }}>
+                  {waitlistArr?.[0]?.[i]}
+                </div>
+                {columns >= 3 && (
+                  <div className="shrink-0" style={{ width }}>
+                    {waitlistArr?.[1]?.[i]}
+                  </div>
+                )}
+                {columns >= 4 && (
+                  <div className="shrink-0" style={{ width }}>
+                    {waitlistArr?.[2]?.[i]}
+                  </div>
+                )}
+              </div>
+            ))}
           </div>
         </div>
-        <div className="flex w-full py-2 border-b-1 border-b-dark-gray">
-          <div className="p-4 w-35 flex-none">Waitlist</div>
-          <div className="p-4">
-            <p>{waitlistArr.join(", ")}</p>
-          </div>
-        </div>
-        <div className="flex w-full py-2">
-          <div className="p-4 w-35 flex-none">Coming soon</div>
-          <div className="p-4">
-            <p>{comingSoonArr.join(", ")}</p>
-          </div>
-        </div>
-      </div>
+      </GlowingEdgeCard>
     </div>
   );
 }
