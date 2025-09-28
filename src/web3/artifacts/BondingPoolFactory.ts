86d44ca31c361e898e48ca598dd1c59040d860f4
volare-dev <dev@volare.finance>
2025-09-28 12:02:52 +0800

    check create pool role

--- src/web3/artifacts/BondingPoolFactory.ts
+++ src/web3/artifacts/BondingPoolFactory.ts
@@ -9,6 +9,20 @@ export const BondingPoolFactoryABI = [
   // },
   // {
   //   type: "function",
+  //   name: "DEFAULT_ADMIN_ROLE",
+  //   inputs: [],
+  //   outputs: [{ name: "", type: "bytes32", internalType: "bytes32" }],
+  //   stateMutability: "view",
+  // },
+  {
+    type: "function",
+    name: "POOL_CREATOR_ROLE",
+    inputs: [],
+    outputs: [{ name: "", type: "bytes32", internalType: "bytes32" }],
+    stateMutability: "view",
+  },
+  // {
+  //   type: "function",
   //   name: "SALE_TOKEN",
   //   inputs: [],
   //   outputs: [{ name: "", type: "address", internalType: "address" }],
@@ -95,6 +109,33 @@ export const BondingPoolFactoryABI = [
   //   outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
   //   stateMutability: "view",
   // },
+  // {
+  //   type: "function",
+  //   name: "getRoleAdmin",
+  //   inputs: [{ name: "role", type: "bytes32", internalType: "bytes32" }],
+  //   outputs: [{ name: "", type: "bytes32", internalType: "bytes32" }],
+  //   stateMutability: "view",
+  // },
+  // {
+  //   type: "function",
+  //   name: "grantRole",
+  //   inputs: [
+  //     { name: "role", type: "bytes32", internalType: "bytes32" },
+  //     { name: "account", type: "address", internalType: "address" },
+  //   ],
+  //   outputs: [],
+  //   stateMutability: "nonpayable",
+  // },
+  {
+    type: "function",
+    name: "hasRole",
+    inputs: [
+      { name: "role", type: "bytes32", internalType: "bytes32" },
+      { name: "account", type: "address", internalType: "address" },
+    ],
+    outputs: [{ name: "", type: "bool", internalType: "bool" }],
+    stateMutability: "view",
+  },
   {
     type: "function",
     name: "increasePoolCap",
@@ -105,6 +146,33 @@ export const BondingPoolFactoryABI = [
     outputs: [],
     stateMutability: "nonpayable",
   },
+  // {
+  //   type: "function",
+  //   name: "renounceRole",
+  //   inputs: [
+  //     { name: "role", type: "bytes32", internalType: "bytes32" },
+  //     { name: "account", type: "address", internalType: "address" },
+  //   ],
+  //   outputs: [],
+  //   stateMutability: "nonpayable",
+  // },
+  // {
+  //   type: "function",
+  //   name: "revokeRole",
+  //   inputs: [
+  //     { name: "role", type: "bytes32", internalType: "bytes32" },
+  //     { name: "account", type: "address", internalType: "address" },
+  //   ],
+  //   outputs: [],
+  //   stateMutability: "nonpayable",
+  // },
+  // {
+  //   type: "function",
+  //   name: "supportsInterface",
+  //   inputs: [{ name: "interfaceId", type: "bytes4", internalType: "bytes4" }],
+  //   outputs: [{ name: "", type: "bool", internalType: "bool" }],
+  //   stateMutability: "view",
+  // },
   // {
   //   type: "event",
   //   name: "PoolCreated",
@@ -120,7 +188,72 @@ export const BondingPoolFactoryABI = [
   //   ],
   //   anonymous: false,
   // },
+  // {
+  //   type: "event",
+  //   name: "RoleAdminChanged",
+  //   inputs: [
+  //     { name: "role", type: "bytes32", indexed: true, internalType: "bytes32" },
+  //     {
+  //       name: "previousAdminRole",
+  //       type: "bytes32",
+  //       indexed: true,
+  //       internalType: "bytes32",
+  //     },
+  //     {
+  //       name: "newAdminRole",
+  //       type: "bytes32",
+  //       indexed: true,
+  //       internalType: "bytes32",
+  //     },
+  //   ],
+  //   anonymous: false,
+  // },
+  // {
+  //   type: "event",
+  //   name: "RoleGranted",
+  //   inputs: [
+  //     { name: "role", type: "bytes32", indexed: true, internalType: "bytes32" },
+  //     {
+  //       name: "account",
+  //       type: "address",
+  //       indexed: true,
+  //       internalType: "address",
+  //     },
+  //     {
+  //       name: "sender",
+  //       type: "address",
+  //       indexed: true,
+  //       internalType: "address",
+  //     },
+  //   ],
+  //   anonymous: false,
+  // },
+  // {
+  //   type: "event",
+  //   name: "RoleRevoked",
+  //   inputs: [
+  //     { name: "role", type: "bytes32", indexed: true, internalType: "bytes32" },
+  //     {
+  //       name: "account",
+  //       type: "address",
+  //       indexed: true,
+  //       internalType: "address",
+  //     },
+  //     {
+  //       name: "sender",
+  //       type: "address",
+  //       indexed: true,
+  //       internalType: "address",
+  //     },
+  //   ],
+  //   anonymous: false,
+  // },
   // { type: "error", name: "RulesLengthMismatch", inputs: [] },
+  // {
+  //   type: "error",
+  //   name: "UnauthorizedPoolCreator",
+  //   inputs: [{ name: "caller", type: "address", internalType: "address" }],
+  // },
   // { type: "error", name: "ZeroSaleToken", inputs: [] },
   // { type: "error", name: "ZeroTreasury", inputs: [] },
 ] as const;
