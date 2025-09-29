2d3eb113bb692c6555df2762a474001dd74f8f9f
volare-dev <dev@volare.finance>
2025-09-29 11:07:38 +0800

    update roadmap image

--- src/app/(frontend)/_components/Roadmap/Mobile/index.tsx
+++ src/app/(frontend)/_components/Roadmap/Mobile/index.tsx
@@ -156,7 +156,7 @@ export default function MobileRoadmap() {
       <div className="roadmap-bg-wrapper">
         <div className="roadmap-bg-container">
           <Image
-            src="/images/bg-roadmap-line.png"
+            src="/images/bg-roadmap.png"
             alt=""
             width={1414}
             height={1491}
