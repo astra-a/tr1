1e0f1048c103cb7d89c98453254b68e8334d5dfb
volare-dev <dev@volare.finance>
2025-09-30 13:42:22 +0800

    update subgraph url

--- src/subgraph/constants.ts
+++ src/subgraph/constants.ts
@@ -1,7 +1,6 @@
 import { holesky } from "viem/chains";
 
 export const SUBGRAPH_URLS: { [key: number]: string } = {
-  // [bsc.id]: "https://api.studio.thegraph.com/query/75174/bonding-pool-subgraph/version/latest",
-  [holesky.id]:
-    "http://18.117.183.132:8000/subgraphs/name/bonding-pool-subgraph",
+  // [bsc.id]: "/bsc/bonding-pool-subgraph",
+  [holesky.id]: "/holesky/bonding-pool-subgraph",
 };
